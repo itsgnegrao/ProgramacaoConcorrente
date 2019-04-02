e51f1f636d4a7496b5fbaae03c0bf9fca2882605
gNegrao <silvag.1996@alunos.utfpr.edu.br>
2019-04-02 19:56:42 -0300

    slide17 ex1

---
+++ src/topico3slide17/Ex1.java
@@ -0,0 +1,32 @@
+package topico3slide17;
+
+import java.util.List;
+
+/**
+ *  Faca um programa em Java que inicie tres threads e, cada
+    thread, espere um tempo aleatoorio para terminar.
+ */
+public class Ex1 {
+    public static void main(String[] args){
+        Thread t1, t2, t3;
+        t1 = new Thread(() -> {
+            System.out.println("Hello from thread 1!"); 
+            try{Thread.sleep(4000);}catch(Exception e){} 
+            System.out.println("Thread 1 Finalizada!");
+        });
+        t2 = new Thread(() -> {
+            System.out.println("Hello from thread 2!"); 
+            try{Thread.sleep(2000);} catch(Exception e){} 
+            System.out.println("Thread 2 Finalizada!");
+        });
+        t3 = new Thread(() -> { 
+            System.out.println("Hello from thread 3!"); 
+            try{Thread.sleep(1000);} catch(Exception e){} 
+            System.out.println("Thread 3 Finalizada!");
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
